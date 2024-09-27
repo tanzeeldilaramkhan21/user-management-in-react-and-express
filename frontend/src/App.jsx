a047f73aa93e5e30c13586cd01f4a5e09f6cc8f7
tanzeeldilaramkhan21 <tanzeelkhandec21@gmail.com>
2024-09-27 20:02:44 +0500

    app function created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,3 +3,18 @@ import axios from 'axios'
 
 const API_URL = 'http://localhost:3000/api/users'
 
+function App() {
+  const [users, setUsers]  = useState([])
+  const [newUser, setNewUser] = useState('');
+  const [updateUser, setUpdateUser] = useState({ id: '', name: '' });
+ 
+  async function fetchUsers(){
+     const response = await axios.get(API_URL)
+     const content = response.data
+     
+     setUsers(content.data)
+  }
+ 
+  useEffect(()=>{
+   fetchUsers()
+  },[])
